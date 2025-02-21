081f55d923be8e23d391811c1e0bb97a23126461
parthanand4054 <parthanand4054@gmail.com>
2025-02-20 19:27:35 -0800

    [Feature] add rectangle to contentView, tweaks to HomeView

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
                 
             }
             
-            
+            Rectangle()
         }
         .padding()
     }
--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var title: String = ""
+    @State private var title: String = "rgergergegerg"
     
     var body: some View {
         VStack {
             Text("Screen 2!")
             Text("Screen 2!")
-            Text("Screen 2!")
+            Text("Screen 3!")
+            
+            Text("Screen 4!")
         }
     }
 }
